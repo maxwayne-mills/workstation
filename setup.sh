39d04dd3a8350226eb4613e8a1d122bc4d288fbf
Clarence Mills <cmills@opensitesolutions.com>
2017-01-17 22:31:09 -0500

    Download script git repository and install developement packages

--- setup.sh
+++ setup.sh
@@ -7,3 +7,14 @@ cd workstation
 cp bash_aliases ~/.bash_aliases
 cp  gitconfig ~/.gitconfig
 ./install_apps.sh
+
+# Clone scripts directory
+cd /mtp
+git clone https://github.com/maxwayne-mills/scripts.git
+cd scripts/shell
+# Install Terraform
+./install_terraform.sh
+
+# Install vagrant
+./install_vagrant.sh
+
